37b8c7f37580930115195925db174335b1c1945a
at@atta <athallah.rafi13@gmail.com>
2024-12-14 15:42:51 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from settings import *
+import moderngl as mgl
+import pygame as pg
+import sys
+
+class VoxelEngine:
+    def __init__(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def render(self):
+        pass
+    
+    def handle_events(self):
+        pass
+    
+    def run(self):
+        pass
+    
+if __name__ == '__main__':
+    app = VoxelEngine()
+    app.run()
